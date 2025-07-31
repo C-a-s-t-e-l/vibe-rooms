245bf6ea80cc75c6f794295d9b0e689198e982c8
Cas <ezzyllena@gmail.com>
2025-07-31 22:25:27 +0800

    fix: Use absolute callbackURL in GoogleStrategy

--- backend/server.js
+++ backend/server.js
@@ -62,7 +62,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: "https://vibes-fqic.onrender.com/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
       return done(null, profile);
